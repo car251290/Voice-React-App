da02611a7a6595f9a1b74535e65dfc59cada03e6
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-12-07 21:41:06 -0800

    Add files via upload

--- Modal.js
+++ Modal.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Typography, Divider, Chip, TextField, Button } from '@material-ui/core';
 import SimpleModal from '@material-ui/core/Modal';
 import emailjs from 'emailjs-com';
-
 import useStyles from './styles';
 
 const Modal = ({ isOpen, setIsOpen, showFeedback }) => {
